bcf5f1e0f0c99f7a91848009d2ac305fd7df4dd7
Khanh Nguyen <khanhnn@nustechnology.com>
2017-01-29 02:27:44 +0700

    small changes to documents

--- app/documents/documents.component.ts
+++ app/documents/documents.component.ts
@@ -18,7 +18,7 @@ export class DocumentsComponent implements OnInit {
   mode = "Observable";
 
   constructor(
-    private documentService: DocumentService;
+    private documentService: DocumentService
   ){}
 
   ngOnInit(){
